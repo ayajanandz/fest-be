65a52bdbd2e9996978a56c478646a820a969cd3e
Ayaj Anand <sam.ayajanand@gmail.com>
2024-10-06 19:07:21 +0530

    added res for bad request

--- mongo/login.js
+++ mongo/login.js
@@ -5,29 +5,27 @@ const { connectDB } = require("./connect.js");
 const login = async (req, res) => {
   const { email, password } = req.body;
   
-
   // CONNECTION WITH COLLECTIONS
-
   let mongoDB = await connectDB();
   let collection = mongoDB.collection("users");
- 
+
   let dbResponse = await collection.findOne({ Email: email });
 
-
-  
   if (dbResponse) {
     if (password === dbResponse.Password) {
       const { Password, ...userWithoutPassword } = dbResponse;
-      
-      // Returning everything to the frontend except the password from the databse
 
+      // Returning everything to the frontend except the password from the database
       console.log(userWithoutPassword);
-      res.send({ body: userWithoutPassword, status: 200,  });
+      res.send({ body: userWithoutPassword, status: 200 });
     } else {
-      res.send({ body: {}, status: 200 });
+      // Password is incorrect
+      console.log("No Match !")
+      res.status(401).send({ message: "Incorrect password" });
     }
   } else {
-    res.send({ message: "User not resgistered" });
+    console.log("No match found!");
+    res.status(404).send({ message: "User not registered" });
   }
 };
 
